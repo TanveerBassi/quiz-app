6dd1adf07042bf7a1295fe1d6300bcd021d16e67
Tanveer Bassi <tanveer13a551@gmail.com>
2022-05-01 16:24:53 +0100

    update quiz functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -73,3 +73,88 @@ function startQuiz() {
   //hide any visible cards, show the question card
   hideCards();
   questionCard.removeAttribute("hidden");
+
+  //assign 0 to currentQuestion when start button is clicked, then display the current question on the page
+  currentQuestion = 0;
+  displayQuestion();
+
+  //set total time depending on number of questions
+  time = questions.length * 10;
+
+  //executes function "countdown" every 1000ms to update time and display on page
+  intervalID = setInterval(countdown, 1000);
+
+  //invoke displayTime here to ensure time appears on the page as soon as the start button is clicked, not after 1 second
+  displayTime();
+}
+
+//reduce time by 1 and display new value, if time runs out then end quiz
+function countdown() {
+  time--;
+  displayTime();
+  if (time < 1) {
+    endQuiz();
+  }
+}
+
+//display time on page
+const timeDisplay = document.querySelector("#time");
+function displayTime() {
+  timeDisplay.textContent = time;
+}
+
+//display the question and answer options for the current question
+function displayQuestion() {
+  let question = questions[currentQuestion];
+  let options = question.options;
+
+  let h2QuestionElement = document.querySelector("#question-text");
+  h2QuestionElement.textContent = question.questionText;
+
+  for (let i = 0; i < options.length; i++) {
+    let option = options[i];
+    let optionButton = document.querySelector("#option" + i);
+    optionButton.textContent = option;
+  }
+}
+
+//behaviour when an answer button is clicked: click event bubbles up to div with id "quiz-options"
+//eventObject.target identifies the specific button element that was clicked on
+document.querySelector("#quiz-options").addEventListener("click", checkAnswer);
+
+//Compare the text content of the option button with the answer to the current question
+function optionIsCorrect(optionButton) {
+  return optionButton.textContent === questions[currentQuestion].answer;
+}
+
+//if answer is incorrect, penalise time
+function checkAnswer(eventObject) {
+  let optionButton = eventObject.target;
+  resultDiv.style.display = "block";
+  if (optionIsCorrect(optionButton)) {
+    resultText.textContent = "Correct!";
+    setTimeout(hideResultText, 1000);
+  } else {
+    resultText.textContent = "Incorrect!";
+    setTimeout(hideResultText, 1000);
+    if (time >= 10) {
+      time = time - 10;
+      displayTime();
+    } else {
+      //if time is less than 10, display time as 0 and end quiz
+      //time is set to zero in this case to avoid displaying a negative number in cases where a wrong answer is submitted with < 10 seconds left on the timer
+      time = 0;
+      displayTime();
+      endQuiz();
+    }
+  }
+
+  //increment current question by 1
+  currentQuestion++;
+  //if we have not run out of questions then display next question, else end quiz
+  if (currentQuestion < questions.length) {
+    displayQuestion();
+  } else {
+    endQuiz();
+  }
+}
